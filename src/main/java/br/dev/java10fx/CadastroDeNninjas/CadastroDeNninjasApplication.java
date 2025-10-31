f631c49c1f94f62fe17c43d73bb5483ad0c248f6
jgprado04 <jgprado04@gmail.com>
2025-10-30 22:05:14 -0300

    first commit

---
+++ src/main/java/br/dev/java10fx/CadastroDeNninjas/CadastroDeNninjasApplication.java
@@ -0,0 +1,13 @@
+package br.dev.java10fx.CadastroDeNninjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNninjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNninjasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/java10fx/CadastroDeNninjas/CadastroDeNninjasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.java10fx.CadastroDeNninjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNninjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
